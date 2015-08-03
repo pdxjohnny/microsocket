52483bf3775b5e1de720bf3e06dafa42c87d1dd1
John Andersen <johnandersenpdx@gmail.com>
2015-08-03 08:53:22 -0700

    Got client working
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- client.go
+++ client.go
@@ -1,43 +1,55 @@
 package main
 
 import (
-  "fmt"
-  "net"
-  "net/url"
-  "net/http"
+	"fmt"
+	"net"
+	"net/http"
+	"net/url"
 
-  "github.com/gorilla/websocket"
+	"github.com/gorilla/websocket"
 )
 
-func Start(url_string string) interface{} {
-  u, err := url.Parse(url_string)
-  if err != nil {
-      return err
-  }
+func Start(url_string string) (ws *websocket.Conn, err error) {
+	u, err := url.Parse(url_string)
+	if err != nil {
+		return nil, err
+	}
 
-  rawConn, err := net.Dial("tcp", u.Host)
-  if err != nil {
-      return err
-  }
+	rawConn, err := net.Dial("tcp", u.Host)
+	if err != nil {
+		return nil, err
+	}
 
-  wsHeaders := http.Header{
-      // "Origin":                   {url},
-      // your milage may differ
-      "Sec-WebSocket-Extensions": {"permessage-deflate; client_max_window_bits, x-webkit-deflate-frame"},
-  }
+	wsHeaders := http.Header{
+		"Sec-WebSocket-Extensions": {"permessage-deflate; client_max_window_bits, x-webkit-deflate-frame"},
+	}
 
-  // wsConn, resp, err := websocket.NewClient(rawConn, u, wsHeaders, 1024, 1024)
-  _, resp, err := websocket.NewClient(rawConn, u, wsHeaders, 1024, 1024)
-  if err != nil {
-      return fmt.Errorf("websocket.NewClient Error: %s\nResp:%+v", err, resp)
-  }
-  fmt.Println("Did it!!")
-  return nil
+	wsConn, resp, err := websocket.NewClient(rawConn, u, wsHeaders, 1024, 1024)
+	if err != nil {
+		fmt.Errorf("websocket.NewClient Error: %s\nResp:%+v", err, resp)
+		return nil, err
+	}
+	return wsConn, nil
+}
+
+func readLoop(conn *websocket.Conn) (err error) {
+	for {
+		_, p, err := conn.ReadMessage()
+		if err != nil {
+			return err
+		}
+		fmt.Println(string(p))
+		// err = conn.WriteMessage(messageType, p)
+		// if err != nil {
+		// 	return err
+		// }
+	}
 }
 
 func main() {
-  err := Start("http://localhost:8080/ws")
-  if err != nil {
-    fmt.Println(err)
-  }
+	ws, err := Start("http://localhost:8080/ws")
+	if err != nil {
+		fmt.Println(err)
+	}
+	readLoop(ws)
 }
